18583b672fb4771b14b7e42262384335ee2e2050
Miłosz Kramek <milosz.kramek@gmail.com>
2025-04-01 23:34:37 +0000

    202504020133

--- resources/views/livewire/product/categories.blade.php
+++ resources/views/livewire/product/categories.blade.php
@@ -3,28 +3,27 @@
         $column_name = "name_$lang";
         $column_slug = "slug_$lang";
     @endphp
-    @if($mode === "list")
+    @if ($mode === 'list')
         <div class="flex flex-col">
-            @foreach($primary_categories as $primary)
-                <livewire:product.expandable-category-item :category="$primary"
-                                                           level="primary"/>
+            @foreach ($primary_categories as $primary)
+                <livewire:product.expandable-category-item :category="$primary" level="primary" />
             @endforeach
         </div>
     @else
         <div class="flex flex-wrap justify-center items-start gap-4 mt-6">
-            @foreach($primary_categories as $category)
-                <x-button
-                    :href='route("products.$lang", ["category_primary" => $category->$column_slug])'
-                    :key="$category->id"
-                    class="shadow-lg h-52 aspect-square bg-primary-500 hover:border-primary-500 rounded-full text-white hover:text-primary-600 font-bold text-center text-xl break-words p-4 flex flex-col justify-center items-center gap-2">
-                    @if($category->icon)
+            @foreach ($primary_categories as $category)
+                <x-button :href='route("products.$lang", ["category_primary" => $category->$column_slug])' :key="$category->id"
+                    class="shadow-lg h-52 aspect-square bg-primary-500 hover:border-primary-500 rounded-full text-white hover:text-primary-600 font-bold text-center text-xl break-words p-4 flex flex-col justify-center items-center gap-2"
+                >
+                    @if ($category->icon)
                         <img
                             src="{{ $category->icon }}"
-                            width="100" height="100"
-                            alt="Obraz kategorii {{ $category->$column_name }}"/>
+                            width="100"
+                            height="100"
+                            alt="Obraz kategorii {{ $category->$column_name }}"
+                        />
                     @endif
-                    <span
-                        class="{{ strlen($category->$column_name) > 20 ? 'text-sm' : '' }}">{{ $category->$column_name }}</span>
+                    <span class="{{ strlen($category->$column_name) > 20 ? 'text-sm' : '' }}">{{ $category->$column_name }}</span>
                 </x-button>
             @endforeach
         </div>
--- resources/views/livewire/product/expandable-category-item.blade.php
+++ resources/views/livewire/product/expandable-category-item.blade.php
@@ -3,12 +3,12 @@
         $column_slug = "slug_$lang";
     @endphp
     <div class="flex justify-start items-center uppercase">
-        @if($availableSecondaryCategories || $availableTertiaryCategories)
+        @if ($availableSecondaryCategories || $availableTertiaryCategories)
             <x-button wire:click="toggle" flat class="!px-2">
-                @if($expanded)
-                    <x-icon name="minus" regular class="w-4"/>
+                @if ($expanded)
+                    <x-icon name="minus" regular class="w-4" />
                 @else
-                    <x-icon name="plus" regular class="w-4"/>
+                    <x-icon name="plus" regular class="w-4" />
                 @endif
             </x-button>
         @else
@@ -16,19 +16,17 @@
         @endif
         <a href='{{ route("products.$lang", $this->getCategories()) }}'>{{ $this->getCategoryName() }}</a>
     </div>
-    @if($availableSecondaryCategories && $expanded)
+    @if ($availableSecondaryCategories && $expanded)
         <div class="pl-8 flex flex-col gap-2">
-            @foreach($this->category->secondLevelCategories as $category)
-                <livewire:product.expandable-category-item :category="$category"
-                                                           level="secondary"/>
+            @foreach ($this->category->secondLevelCategories as $category)
+                <livewire:product.expandable-category-item :category="$category" level="secondary" />
             @endforeach
         </div>
     @endif
-    @if($availableTertiaryCategories && $expanded)
+    @if ($availableTertiaryCategories && $expanded)
         <div class="pl-8 flex flex-col gap-2">
-            @foreach($this->category->thirdLevelCategories as $category)
-                <livewire:product.expandable-category-item :category="$category"
-                                                           level="tertiary"/>
+            @foreach ($this->category->thirdLevelCategories as $category)
+                <livewire:product.expandable-category-item :category="$category" level="tertiary" />
             @endforeach
         </div>
     @endif
