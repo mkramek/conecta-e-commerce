46939cf8cb401be81db333e7a7db63ce8449dd1e
Miłosz Kramek <milosz.kramek@gmail.com>
2025-04-01 09:18:14 +0000

    202504011118

--- storage/framework/views/91118f398c7581390fee20b1efcb8c21.php
+++
@@ -1,2 +0,0 @@
-<input <?php echo e($attributes->merge(['type' => 'hidden'])); ?> />
-<?php /**PATH /var/app/ecommerce/vendor/wireui/wireui/src/Components/Wrapper/views/components/hidden.blade.php ENDPATH**/ ?>
